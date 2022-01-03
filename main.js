6fe457e939fedfb6749096f2a82c89b5a63ff875
J-Kagotho <joycekagotho2002@gmail.com>
2022-01-03 08:45:19 -0500

    add a submit button

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-$(Document).ready(function){
-    $(".design").on("click",function(){
+$(Document).ready(function(){
+    $(".dtext").on("click",function(){
        $(".pointer").toogle();
-       $(".designtext").toogle();
+       $(".ptext").toogle();
+    });   
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("form#form").submit(function(event){
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var message = $("textarea#message").val();
+        if(name && email){
+            alert (name + ",message received. We will contact you soon.");
+        }
+        else{
+            alert ("Please enter you name and email!");
+        }
+    });
 });
