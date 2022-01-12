1c4c4aca8877c2ddc13142bbb6aa2617b13781e4
J-Kagotho <joycekagotho2002@gmail.com>
2022-01-12 16:08:41 -0500

    add modification to the files

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
-  $(Document).ready(function(){
-    $(".des").on("click", function(){
-       $(".dein").toggle();
-       $(".detext").toogle();
+  $(document).ready(function(){
+    $(".click").on("click", function(){
+       $(this).children('.icons').toggleClass('inactive');
+       $(this).children('.message').toggleClass('active');
     }); 
 });  
-
-   $(Document).ready(function(){
-    $(".dev").on("click", function(){
-       $(".devin").toogle();
-       $(".devtext").toogle();
+     
+    $(document).ready(function(){
+        $("form#fill").submit(function(event){
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#area").val();
+            if(name && email){
+                alert (name + ",message received. We will contact you soon.");
+            }
+            else{
+                alert ("Please enter you name and email!");
+            }
+        });
     });
-});
-
-  $(Document).ready(function(){
-    $(".pro").on("click", function(){
-        $(".prin").toogle();
-        $(".ptext").toogle();
-    }); 
- });       
-
-$(document).ready(function(){
-    $("form#fill").submit(function(event){
-        var name = $("input#name").val();
-        var email = $("input#email").val();
-        var message = $("textarea#area").val();
-        if(name && email){
-            alert (name + ",message received. We will contact you soon.");
-        }
-        else{
-            alert ("Please enter you name and email!");
-        }
-    });
-});
 
